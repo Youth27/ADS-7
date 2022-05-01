5efd4dd636302dc3eb5880fcfa601b3b8adffbc2
Youth27 <94329773+Youth27@users.noreply.github.com>
2022-05-01 07:54:07 +0300

    Артём Маричев

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,8 +4,63 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+  private:
+  struct ITEM {
+   T znach;
+   ITEM* next;
 };
+ITEM* first, *last;
+  public:
+   TPQueue();
+   void push(T nznach);
+   T pop();
+};
+
+template <typename T>
+TPQueue<T>::TPQueue() {
+  first, last = 0;
+}
+
+template <typename T>
+void TPQueue<T>::push(T nznach) {
+  ITEM* last = first;
+  if (!last) {
+    last = new ITEM;
+    first = last;
+    last -> znach = nznach;
+    last -> next = nullptr;
+  } else {
+    ITEM* t = new ITEM;
+    t -> znach = nznach;
+    if (first -> znach.prior <= t -> znach.prior) {
+      t -> next = last;
+      first = t;
+    }
+    
+    while (last != nullptr) {
+      if ((last -> znach.prior == t -> znach.prior &&
+           (last -> next == nullptr ||
+            last -> next -> znach.prior < t -> znach.prior)) ||
+          (last -> znach.prior > t -> znach.prior &&
+           ((last -> next != nullptr &&
+             last -> next -> znach.prior < t -> znach.prior) ||
+            (last -> next == nullptr)))) {
+        t -> next = last -> next;
+        last -> next = t;
+        return;
+      }
+      last = last->next;
+    }
+    
+    }
+}
+
+template <typename T>
+T TPQueue<T>::pop() {
+  ITEM* t = first;
+  first = first -> next;
+  return (t -> znach);
+}
 
 struct SYM {
   char ch;
